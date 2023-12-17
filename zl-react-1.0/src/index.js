b2fcd00c5e846ca2e6f9f989e7f1e9b4826baae1
ZL <ray040406@gmail.com>
2023-12-17 16:56:34 +1300

    adding styles for 3 parts + classNames

--- zl-react-1.0/src/index.js
+++ zl-react-1.0/src/index.js
@@ -3,21 +3,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-
 //JSX element, header
+// CSS inline styling
+const headerStyles = {
+  backgroundColor: '#5ED6FF',
+  fontFamily: 'Helvetica Neue',
+  padding: 25,
+  lineHeight: 1.5,
+}
+
 const header = (
-  <header>
-    <h1>Welcome to 30 Days Of React</h1>
-    <h2>Getting Started React</h2>
-    <h3>JavaScript Library</h3>
-    <p>zlrkw11</p>
-    <small>Dec 17, 2023</small>
+  <header style={headerStyles}>
+    <div className='header-wrapper'>
+      <h1>Welcome to 30 Days Of React</h1>
+      <h2>Getting Started React</h2>
+      <h3>JavaScript Library</h3>
+      <p>zlrkw11</p>
+      <small>Dec 17, 2023</small>
+    </div>
   </header>
 )
 
 //JSX element main
+const mainStyles = {
+  backgroundColor: '#F3F0F5',
+}
 const main = (
-  <main>
+  <main style={mainStyles}>
     <p>Prerequisie to get started react.js</p>
     <ul>
       <li>HTML</li>
@@ -28,15 +40,18 @@ const main = (
 )
 
 // JSX element, footer
+const footerStyles = {
+  backgroundColor: '#61DBFB',
+}
 const footer = (
-  <footer>
+  <footer style={footerStyles}>
     <p>Copyright 2020</p>
   </footer>
 )
 
 // JSX element, app, a container / parent
 const app = (
-  <div>
+  <div className='app'>
     {header}
     {main}
     {footer}
