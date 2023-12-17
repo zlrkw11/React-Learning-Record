7157c805293e50f47115284d870ab883e9364517
ZL <ray040406@gmail.com>
2023-12-17 16:50:49 +1300

    3 parts + App container

--- zl-react-1.0/src/index.js
+++ zl-react-1.0/src/index.js
@@ -1,14 +1,47 @@
+//import the react & react-Dom package
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+
+//JSX element, header
 const header = (
   <header>
     <h1>Welcome to 30 Days Of React</h1>
     <h2>Getting Started React</h2>
     <h3>JavaScript Library</h3>
+    <p>zlrkw11</p>
+    <small>Dec 17, 2023</small>
   </header>
 )
 
-const rootElement = document.getElementById('root')
+//JSX element main
+const main = (
+  <main>
+    <p>Prerequisie to get started react.js</p>
+    <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+    </ul>
+  </main>
+)
 
-ReactDOM.render(header, rootElement);
+// JSX element, footer
+const footer = (
+  <footer>
+    <p>Copyright 2020</p>
+  </footer>
+)
+
+// JSX element, app, a container / parent
+const app = (
+  <div>
+    {header}
+    {main}
+    {footer}
+  </div>
+)
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(app, rootElement);
