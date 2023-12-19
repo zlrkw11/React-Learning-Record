437a8ed0d8d36fcccdd492d0a907c2f4ed705453
ZL <ray040406@gmail.com>
2023-12-19 15:59:32 +1300

    created D5

--- zl-react-1.0/src/index.js
+++ zl-react-1.0/src/index.js
@@ -1,62 +1,27 @@
-//import the react & react-Dom package
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-
-//JSX element, header
-// CSS inline styling
-const headerStyles = {
-  backgroundColor: '#5ED6FF',
-  fontFamily: 'Helvetica Neue',
-  padding: 25,
-  lineHeight: 1.5,
+// Hexadecimal color generator
+const hexaColor = () => {
+  let str = '0123456789abcdef'
+  let color = ''
+  for (let i = 0; i < 6; i++) {
+    let index = Math.floor(Math.random() * str.length)
+    color += str[index]
+  }
+  return '#' + color
 }
 
-const header = (
-  <header style={headerStyles}>
-    <div className='header-wrapper'>
-      <h1>Welcome to 30 Days Of React</h1>
-      <h2>Getting Started React</h2>
-      <h3>JavaScript Library</h3>
-      <p>zlrkw11</p>
-      <small>Dec 17, 2023</small>
-    </div>
-  </header>
-)
+const HexaColor = () => <div>{hexaColor()}</div>
 
-//JSX element main
-const mainStyles = {
-  backgroundColor: '#F3F0F5',
-}
-const main = (
-  <main style={mainStyles}>
-    <p>Prerequisie to get started react.js</p>
-    <ul>
-      <li>HTML</li>
-      <li>CSS</li>
-      <li>JavaScript</li>
-    </ul>
-  </main>
-)
-
-// JSX element, footer
-const footerStyles = {
-  backgroundColor: '#61DBFB',
-}
-const footer = (
-  <footer style={footerStyles}>
-    <p>Copyright 2020</p>
-  </footer>
-)
-
-// JSX element, app, a container / parent
-const app = (
-  <div className='app'>
-    {header}
-    {main}
-    {footer}
-  </div>
+const AllColors = () => (
+  <HexaColor/>,
+  <HexaColor/>,
+  <HexaColor/>,
+  <HexaColor/>,
+  <HexaColor/>
 )
 
 const rootElement = document.getElementById('root')
-ReactDOM.render(app, rootElement);
+// we render the App component using the ReactDOM package
+ReactDOM.render(<AllColors/>, rootElement)
