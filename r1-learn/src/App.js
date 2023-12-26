88eea596e0831859ca2ff35f56be669efd7d36eb
ZL <ray040406@gmail.com>
2023-12-26 21:32:52 +1300

    name & name html

--- r1-learn/src/App.js
+++ r1-learn/src/App.js
@@ -1,9 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
 function App() {
+  const name = "Ray";
+
+  // make a already-done HTML name
+  const name2 = <h1>Ray</h1>
+  
   return (
-    <div className="App"></div>
+    <div className="App">
+      <h1>{name}</h1>
+    
+    </div>
   );
 }
 
