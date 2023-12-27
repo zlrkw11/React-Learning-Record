769e5fa53a2aadff2e8834ff44684a7825ca6a4e
ZL <ray040406@gmail.com>
2023-12-27 21:42:36 +1300

    inline-css in jsx

--- r1-learn/src/D3.js
+++ r1-learn/src/D3.js
@@ -3,8 +3,17 @@ import "./App.css";
 // ternary operators
 
 function App(){
-    const age = 15
-    return <div className="App">{age >= 18 ? <h1>OVER AGE</h1> : <h1>UNDER AGE</h1>}</div>
+    const age = 15;
+    const isGreen = false;
+
+    return (    
+    <div className="App">
+        {age >= 18 ? <h1>OVER AGE</h1> : <h1>UNDER AGE</h1>}
+        <h1 style={{color: "red", backgroundColor: "black"}}>THIS HAS COLOR</h1>
+    </div>
+)
+
+
 }
 
 
