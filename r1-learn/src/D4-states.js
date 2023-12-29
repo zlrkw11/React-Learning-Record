4c8d5be01e34e5b5f2788024949184b1f1c15930
ZL <ray040406@gmail.com>
2023-12-29 18:01:29 +1300

    event function

--- r1-learn/src/D4-states.js
+++ r1-learn/src/D4-states.js
@@ -3,9 +3,15 @@ import "./App.css"
 function App(){
 
     const age = 0
+
+    // event function to pass
+    const increaseAge = () => {
+        age = age + 1;
+    }
+    
     return (
         <div className="App">{age}
-        <button onClick={age+1}> Increase Age </button>
+        <button onClick={}> Increase Age </button>
         </div>
         )
 
