da3eea02fa1f45994452da9630cf8d1a6bdfdc79
ZL <ray040406@gmail.com>
2023-12-30 20:48:34 +1300

    showing the state function controls the fvalue

--- r1-learn/src/D5-CRUD-to-do-list.js
+++ r1-learn/src/D5-CRUD-to-do-list.js
@@ -1,12 +1,23 @@
 import './App.css'
+import {useState} from 'react'
 
 // 1st CRUD 
 function App(){
-    
+    // list empty at the initial state
+    const [todoList, setTodoList] = useState([]);
+
+    // get input value
+    const [newTask, setNewTask] = useState("");
+
+    const handleChange = (event) => {
+        // setNewTask sets newTask's value
+        setNewTask = event.target.value;
+    };
+
     return (
         <div className="App">
             <div className="addTask">
-                <input />
+                <input onChange={handleChange}/>
                 <button>Add Task</button>
             </div>
             <div className="list"></div>
