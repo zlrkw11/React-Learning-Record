ce413d09113d73a9d71185509beff94ce2521020
ZL <ray040406@gmail.com>
2023-12-30 20:44:33 +1300

    structure

---
+++ r1-learn/src/D5-CRUD-to-do-list.js
@@ -0,0 +1,18 @@
+import './App.css'
+
+// 1st CRUD 
+function App(){
+    
+    return (
+        <div className="App">
+            <div className="addTask">
+                <input />
+                <button>Add Task</button>
+            </div>
+            <div className="list"></div>
+        </div>
+    )
+
+}
+
+export default App
--- r1-learn/src/index.js
+++ r1-learn/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './D4-exercise'
+import App from './D5-CRUD-to-do-list'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
