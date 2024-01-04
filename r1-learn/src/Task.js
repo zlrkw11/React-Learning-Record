4a5c9b74bd2655405a2ab54caf2cf47985fce21a
zlrkw11 <ray040406@gmail.com>
2024-01-04 13:55:38 +0800

    complete button

--- r1-learn/src/Task.js
+++ r1-learn/src/Task.js
@@ -1,8 +1,9 @@
-const Task = (props) => {
+export const Task = (props) => {
 
     return (
     <div>
         <h1>{props.taskName}</h1>
+        <button> Complete </button>
         <button onClick={()=> deleteTask(props.id)}>x</button>
     </div>
     );
