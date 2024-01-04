1f16ded36df97bd65a6c0b4a78d12e5f0fde8af8
zlrkw11 <ray040406@gmail.com>
2024-01-04 19:50:09 +0800

    D7

---
+++ r1-learn/src/D7.js
@@ -0,0 +1,14 @@
+// CAT DATA FETCH API 
+import "./App.css";
+
+function App() {
+    return (
+
+        <div className="App"> 
+            <button> Generate Cat Fact </button>
+            <p></p>
+        </div>
+    )
+}
+
+export default App;
--- r1-learn/src/storage/D6-text.js
+++ r1-learn/src/storage/D6-text.js
@@ -15,7 +15,6 @@ export const Text = () => {
         }
     }, [])
 
-
     return (
 
     <div> 
